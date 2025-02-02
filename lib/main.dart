803be256b960ad4b7b4e6e58b95c0a4c2e4edb3c
yzemmouri <umpyassinez@gmail.com>
2025-02-02 10:35:23 +0100

    create the blank screen of the game

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,7 @@
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+  final game = FlameGame();
+  runApp(GameWidget(game: game));
 }
