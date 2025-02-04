e026aa45fe62793be85f5da298a7587645486b00
yzemmouri <umpyassinez@gmail.com>
2025-02-04 15:49:33 +0100

    feat: display game in full screen feat: set game in landscame orientaion

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'src/krabalo.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+// Set fullscreen mode
+  await SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+
+// Lock to landscape orientation
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeRight,
+    DeviceOrientation.landscapeLeft,
+  ]);
+
   final game = Krabalo();
   runApp(GameWidget(game: game));
 }
